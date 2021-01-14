a4712267cf661cc0c2d44a062933c64dbee098cb
Fernando Chaves <fernando.chaves16@hotmail.com>
2021-01-14 10:44:56 -0300

    Parte 1 finalizada

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
 const form = document.getElementById('form');
-const name = document.getElementById('name');
+const nome = document.getElementById('name');
 const username = document.getElementById('username');
+const email = document.getElementById('email');
 const password = document.getElementById('password');
 const confirmPassword = document.getElementById('confirmPassword');
+const button = document.getElementById('form-button');
 
 
+// Show success
 function showSuccess(input) {
-    
+    input.classList.add('success');
+}
 
-    input.parentElement.classList.add('success');
+// Show error
+function showError(input) {
+    input.classList.add('error');  
+
+    input.nextSibling.nextSibling.classList.add('error');
+    
+}
+
+// Show error message
+function errorMessage() {
+    var small 
 }
 
 
+button.addEventListener('click', function(e){
+    e.preventDefault(e);
 
+    showError(username);
+    showError(password);
 
-form.addEventListener('click', function(e){
-    e.preventDefault();
+    const inputItems = [nome,username,email,password,confirmPassword];
+    
+    inputItems.forEach(input =>   {
 
-    showSuccess(username);
+        
+    });
 })
