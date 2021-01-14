e07a2fcca303c5d663d2702cd821b59eed9d82ee
Fernando Chaves <fernando.chaves16@hotmail.com>
2021-01-14 14:32:19 -0300

    Parte 2 finalizada

--- script.js
+++ script.js
@@ -13,29 +13,57 @@ function showSuccess(input) {
 }
 
 // Show error
-function showError(input) {
+function showError(input, message) {
     input.classList.add('error');  
 
     input.nextSibling.nextSibling.classList.add('error');
+
+    input.nextSibling.nextSibling.innerText = message;
+}
+
+//Check name, username & password length
+function checkLength(nome, username, password) {
+
+    //Name
+    if(nome.value.length < 3) {
+        showError(nome, 'Nome muito curto');
+    } else if (nome.value.length > 16) {
+        showError(nome, 'Nome muito longo');
+    } else {
+        showSuccess(nome);
+    }
+
+    //Username
+    if(username.value.length < 5) {
+        showError(username, 'Username muito curto');
+    } else if (username.value.length > 20) {
+        showError(username, 'Username muito longo');
+    } else {
+        showSuccess(username);
+    }
+
+    //Password
+    if(password.value.length < 6) {
+        showError(password, 'Password muito curto');
+    } else if (password.value.length > 16) {
+        showError(password, 'Password muito longo');
+    } else {
+        showSuccess(password);
+    }
+    
+} 
+    
+
+// Check Email
+function checkEmail() {
     
 }
 
-// Show error message
-function errorMessage() {
-    var small 
-}
 
-
-button.addEventListener('click', function(e){
+form.addEventListener('submit', function(e){
     e.preventDefault(e);
 
-    showError(username);
-    showError(password);
+    checkLength(nome, username, password);
+    checkEmail(email);
 
-    const inputItems = [nome,username,email,password,confirmPassword];
-    
-    inputItems.forEach(input =>   {
-
-        
-    });
-})
+})  
